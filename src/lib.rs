8f5e3ba582a53c03ceddea40413bcb5cf48670dd
chadat23 <chad.derosier@tutanota.com>
2023-11-12 11:50:03 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -64,7 +64,7 @@ impl Device {
         match device {
             Ok(d) => Ok(d),
 //            Err(e) => Err(format!("{}: couldn't deserialize device json", e.clone()).as_str())
-            Err(e) => Err(e.to_string()),
+            Err(e) => Err(e.to_string().as_str()),
         }
     }
 
