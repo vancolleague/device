f8a35f1f2ab889b03ecae5bdb652e5a60c7cfffb
Chad DeRosier <chad.derosier@tutanota.com>
2024-02-21 20:26:52 -0800

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::default::Default;
+use std::mem::discriminant;
+use std::sync::{Arc, Mutex};
 
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -7,165 +9,181 @@ use uuid::Uuid;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
 pub enum Behavior {
-    OnOff,
-    ReversableOnOff,
+    TwoWaySwitch,
+    ThreeWaySwitch,
+    FourWaySwitch,
+    FiveWaySwitch,
+    SixWaySwitch,
+    SevenWaySwitch,
+    EgithWaySwitch,
     Slider,
     ReversableSlider,
+    HVAC,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
 }
 
-pub const DEVICE_TYPES: [(DeviceType, &'static str, u128); 3] = [
-    (
-        DeviceType::Light,
-        "lights",
-        0xf1d34301c91642a88c7c274828177649,
-    ),
-    (DeviceType::Fan, "fans", 0x3d39295fb06842ecabeed69e0d65c105),
-    (
-        DeviceType::Generic,
-        "generic",
-        0x36715f57d8c6400d91f403cc1f20c793,
-    ),
+struct DeviceSynonyms {
+    device_group: DeviceGroup,
+    name: &'static str,
+    uuid_number: u128,
+}
+
+pub const DEVICE_GROUPS: [DeviceSynonyms; 2] = [
+    DeviceSynonyms {
+        device_group: DeviceGroup::Light,
+        name: "lights",
+        uuid_number: 0xf1d34301c91642a88c7c274828177649,
+    },
+    DeviceSynonyms {
+        device_group: DeviceGroup::Fan,
+        name: "fans",
+        uuid_number: 0x3d39295fb06842ecabeed69e0d65c105,
+    }
 ];
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
-pub enum DeviceType {
+pub enum DeviceGroup{
     Light,
     Fan,
-    Generic,
 }
 
-const ACTIONS: [(Action, &'static str, u128); 8] = [
-    (Action::On, "on", 0x928e9b929939486b998d69613f89a9a6),
-    (Action::Off, "off", 0x13df417d74d2443b87e3de60557b75b8),
-    (
-        Action::Up { amount: None },
-        "up",
-        0xbc6c6eeba0ba40e0a57ff5186d4350ce,
-    ),
-    (
-        Action::Down { amount: None },
-        "down",
-        0x62865402c86245eea282d4f2ca8fd51b,
-    ),
-    (Action::Min, "minimum", 0x4aad1b26ea9b455190d0d917102b7f36),
-    (Action::Max, "maximum", 0x4ffb631fa4ba4fb5a189f7a3bb9dfa01),
-    (
-        Action::Reverse,
-        "reverse",
-        0xa201801c1cbe4c918873c04486d3208b,
-    ),
-    (
-        Action::Set { target: 0 },
-        "set",
-        0x2a4fae8107134e1fa8187ac56e4f13e4,
-    ),
+struct ActionSynonyms {
+    action: Action,
+    name: &'static str,
+    uuid_number: u128,
+}
+
+const ACTION_SYNONYMS: [ActionSynonyms; 8] = [
+    ActionSynonyms {
+        action: Action::On,
+        name: "on",
+        uuid_number: 0x928e9b929939486b998d69613f89a9a6,
+    },
+    ActionSynonyms {
+        action: Action::Off,
+        name: "off",
+        uuid_number: 0x13df417d74d2443b87e3de60557b75b8,
+    },
+    ActionSynonyms {
+        action: Action::Up(None),
+        name: "up",
+        uuid_number: 0xbc6c6eeba0ba40e0a57ff5186d4350ce,
+    },
+    ActionSynonyms {
+        action: Action::Down(None),
+        name: "down",
+        uuid_number: 0x62865402c86245eea282d4f2ca8fd51b,
+    },
+    ActionSynonyms {
+        action: Action::Min,
+        name: "minimum",
+        uuid_number: 0x4aad1b26ea9b455190d0d917102b7f36,
+    },
+    ActionSynonyms {
+        action: Action::Max,
+        name: "maximum",
+        uuid_number: 0x4ffb631fa4ba4fb5a189f7a3bb9dfa01,
+    },
+    ActionSynonyms {
+        action: Action::Reverse,
+        name: "reverse",
+        uuid_number: 0x1a8a1df0523e4acb8390b872329a9ca7,
+    },
+    ActionSynonyms {
+        action: Action::Set(0),
+        name: "set",
+        uuid_number: 0x2a4fae8107134e1fa8187ac56e4f13e4,
+    },
 ];
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
 pub enum Action {
     On,
     Off,
-    Up { amount: Option<usize> },
-    Down { amount: Option<usize> },
+    Up(Option<usize>),
+    Down(Option<usize>),
     Min,
     Max,
     Reverse,
-    Set { target: usize },
+    Set(usize),
 }
 
 impl Action {
+    fn same_variant(&self, other: &Self) -> bool {
+        discriminant(self) == discriminant(other)
+    }
+
     pub fn from_str(s: &str, target: Option<usize>) -> Result<Self, &'static str> {
         let s = s.to_lowercase();
 
-        if s == "up" {
-            return Ok(Action::Up { amount: target });
-        }
-
-        if s == "down" {
-            return Ok(Action::Down { amount: target });
-        }
-
-        if s == "set" && target.is_some() {
-            return Ok(Action::Set {
-                target: target.unwrap(),
-            });
-        }
-
-        let action_set: HashMap<&str, Action> = ACTIONS.iter().map(|(a, t, _)| (*t, *a)).collect();
-
-        for (key, &value) in action_set.iter() {
-            if s.starts_with(key) {
-                return Ok(value);
+        match s.as_str() {
+            "up" => {
+                return Ok(Action::Up(target));
+            }
+            "down" => {
+                return Ok(Action::Down(target));
+            }
+            "set" => {
+                if target.is_some() {
+                    return Ok(Action::Set(target.unwrap()));
+                } else {
+                    return Err("No target was given");
+                }
+            }
+            name => {
+                for action in ACTION_SYNONYMS {
+                    if action.name == name {
+                        return Ok(action.action);
+                    }
+                }
             }
         }
-
         Err("Bad Action name given")
     }
 
-    pub fn to_str(&self) -> &str {
-        match *self {
-            Action::Up{ .. } => "up",
-            Action::Down { .. } => "down",
-            Action::Set { .. } => "set",
-            _ => {
-                for a in ACTIONS {
-                    if a.0 == *self {
-                        return a.1;
-                    }
-                }
-                ""
+    pub fn from_u128(uuid_number: u128, target: Option<usize>) -> Result<Self, &'static str> {
+        for action_synonym in ACTION_SYNONYMS {
+            if action_synonym.uuid_number == uuid_number {
+                return Self::from_str(action_synonym.name, target);
             }
         }
-    }
-
-    pub fn from_u128(num: u128, target: Option<usize>) -> Result<Self, &'static str> {
-        if ACTIONS[7].2 == num && target.is_some() {
-            let target = target.unwrap();
-            if target > 7 {
-                return Err("Target is too large");
-            } else {
-                return Ok(Action::Set { target: target });
-            }
-        }
-
-        for (a, _, n) in ACTIONS.iter() {
-            if *n == num {
-                return Ok(a.clone());
-            }
-        }
-
         Err("Bad Uuid number given, no associated action")
     }
 
-    pub fn to_uuid(&self) -> Uuid {
-        for (a, _, n) in ACTIONS.iter() {
-            if a == self {
-                return Uuid::from_u128(n.clone());
+    pub fn to_str(&self) -> &'static str {
+        use Action as A;
+        for action_synonym in ACTION_SYNONYMS {
+            if self.same_variant(&action_synonym.action) {
+                return action_synonym.name;
             }
         }
-        Uuid::from_u128(1)
+        ""
     }
 
-    pub fn get_target(&self) -> Option<usize> {
-        match self {
-            Action::Set { target: a } => Some(a.clone()),
-            _ => None,
+    pub fn to_uuid(&self) -> Uuid {
+        use Action as A;
+        for action_synonym in ACTION_SYNONYMS {
+            if self.same_variant(&action_synonym.action) {
+                return Uuid::from_u128(action_synonym.uuid_number);
+            }
         }
+        Uuid::from_u128(0x0)
     }
 
-    fn get_amount(&self) -> Option<usize> {
+    pub fn get_value(&self) -> Option<usize> {
         match self {
-            Action::Up { amount: a } => a.clone(),
-            Action::Down { amount: a } => a.clone(),
-            _ => None,
-        }
-    }
-
-    pub fn get_target_or_amount(&self) -> Option<usize> {
-        match self {
-            Action::Up { amount: a } => a.clone(),
-            Action::Down { amount: a } => a.clone(),
-            Action::Set { target: a } => Some(a.clone()),
+            Action::Up(v) => v.clone(),
+            Action::Down(v) => v.clone(),
+            Action::Set(v) => Some(v.clone()),
             _ => None,
         }
     }
@@ -183,6 +201,8 @@ impl Action {
 /// let device = Device::new("fan", Uuid::from_u128(0xf1d34301c91642a88c7c274828177649));
 /// println!("Device: {:?}", device);
 /// `
+/// could calculate max number of duty_cycles, check that they're in the right order, check that
+/// available actions Set has the right default value
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
 pub struct Device {
     /// Unique identifier for the device.
@@ -211,7 +231,7 @@ pub struct Device {
     ///
     /// Devaults to [0, 2, 4, 8, 16, 32, 64, 96]. 100 can cause problems for some hardware.
     /// Must be exactly 8 cells long and each cell must be in the inclusive range of 0 though 100. Can be set using 'with_duty_cycles'
-    pub duty_cycles: [u32; 8],
+    pub duty_cycles: [Option<u32>; 8],
     /// The index of the duty cycle from the 'duty_cycles' array that's currently to be targetted.
     ///
     /// Defaults to 3. Must by in the inclusive 0 to 7 range. Can be set using 'with_target'.
@@ -224,7 +244,7 @@ pub struct Device {
     ///
     /// Defaults to Generic which is meant to be used for devices that aren't to be grouped. Can be
     /// set using 'with_device_type'.
-    pub device_type: DeviceType,
+    pub device_group: Option<DeviceGroup>,
     /// Used for controlling the directon of reversable devices.
     ///
     /// Could be used for the direction of a fan or Heat vs. Cool in an HVAC system.
@@ -251,17 +271,17 @@ impl Default for Device {
             available_actions: Vec::from([
                 Action::On,
                 Action::Off,
-                Action::Up { amount: None },
-                Action::Down { amount: None },
+                Action::Up(None),
+                Action::Down(None),
                 Action::Min,
                 Action::Max,
-                Action::Set { target: 0 },
+                Action::Set(0),
             ]),
             default_target: 3,
-            duty_cycles: [0, 2, 4, 8, 16, 32, 64, 96],
+            duty_cycles: [Some(0), Some(2), Some(4), Some(8), Some(16), Some(32), Some(64), Some(96)],
             target: 0,
-            freq_Hz: 1000,
-            device_type: DeviceType::Generic,
+            freq_Hz: 100,
+            device_group: None,
             reversed: false,
             updated: true,
             behavior: Behavior::Slider,
@@ -279,12 +299,13 @@ impl Device {
         action: Action,
         available_actions: Vec<Action>,
         default_target: usize,
-        duty_cycles: [u32; 8],
+        duty_cycles: [Option<u32>; 8],
         target: usize,
-        freq_kHz: u32,
-        device_type: DeviceType,
+        freq_Hz: u32,
+        device_group: Option<DeviceGroup>,
         reversed: bool,
         updated: bool,
+        behavior: Behavior,
     ) -> Self {
         Self {
             uuid,
@@ -293,21 +314,25 @@ impl Device {
             available_actions: Vec::from([
                 Action::On,
                 Action::Off,
-                Action::Up { amount: None },
-                Action::Down { amount: None },
+                Action::Up(None),
+                Action::Down(None),
                 Action::Min,
                 Action::Max,
-                Action::Set { target: 0 },
+                Action::Set(0),
             ]),
-            default_target: 3,
-            duty_cycles: [0, 2, 4, 8, 16, 32, 64, 96],
-            target: 0,
-            freq_Hz: 1000,
-            device_type: DeviceType::Generic,
-            reversed: false,
+            default_target,
+            duty_cycles,
+            target,
+            freq_Hz,
+            device_group,
+            reversed,
             updated: true,
-            behavior: Behavior::Slider,
+            behavior,
         }
+    }
+
+    fn get_max_usable_duty_cycle_index(&self) -> usize {
+        self.duty_cycles.iter().filter(|x| x.is_some()).count() - 1
     }
 
     pub fn from_json(json: &String) -> Result<Self, &'static str> {
@@ -315,7 +340,6 @@ impl Device {
         match device {
             Ok(d) => Ok(d),
             Err(e) => {
-                println!("000000000000 {}", e.to_string().as_str());
                 Err("Could not convert Device to json")
             }
         }
@@ -331,84 +355,68 @@ impl Device {
     }
 
     pub fn take_action(&mut self, action: Action) -> Result<(), &'static str> {
-        if let Action::Up { .. } = action {
-            let mut up_present = false;
-            for a in self.available_actions.iter() {
-                if let Action::Up { .. } = a {
-                    up_present = true;
-                }
-            }
-            if !up_present {
-                return Err("Action not available for device");
-            }
-        } else if let Action::Down { .. } = action {
-            let mut down_present = false;
-            for a in self.available_actions.iter() {
-                if let Action::Down { .. } = a {
-                    down_present = true;
-                }
-            }
-            if !down_present {
-                return Err("Action not available for device");
-            }
-        } else if let Action::Set { .. } = action {
-            let mut set_present = false;
-            for a in self.available_actions.iter() {
-                if let Action::Set { .. } = a {
-                    set_present = true;
-                }
-            }
-            if !set_present {
-                return Err("Action not available for device");
-            }
-        } else if !self.available_actions.contains(&action) {
-            return Err("Action not available for device");
-        }
-        use Action::*;
+        use Action as A;
         match action {
-            On => {
+            A::On => {
+                if !self.available_actions.contains(&action) {
+                    panic!("Action not available fror device");
+                }
                 self.target = self.default_target;
             }
-            Off => {
+            A::Off => {
+                if !self.available_actions.contains(&action) {
+                    panic!("Action not available fror device");
+                }
                 self.target = 0;
             }
-            Up { .. } => {
-                let amount = action.get_amount();
-                let amount = match amount {
+            A::Up(v) => {
+                if !self.available_actions.contains(&Action::Up(None)) {
+                    panic!("Action not available fror device");
+                }
+                let amount = match v {
                     Some(a) => a,
                     None => 1,
                 };
-                self.target = (self.target + amount).min(self.duty_cycles.len() - 1);
+                self.target = (self.target + amount)
+                    .min(self.get_max_usable_duty_cycle_index());
             }
-            Down { .. } => {
-                let amount = action.get_amount();
-                let amount = match amount {
+            A::Down(v) => {
+                if !self.available_actions.contains(&Action::Down(None)) {
+                    panic!("Action not available fror device");
+                }
+                let amount = match v {
                     Some(a) => a,
                     None => 1,
                 };
-                // can't use the "Up" process because it'll underflow sometimes
                 self.target = if amount < self.target {
                     self.target - amount
                 } else {
                     0
                 };
             }
-            Min => {
+            A::Min => {
+                if !self.available_actions.contains(&action) {
+                    panic!("Action not available fror device");
+                }
                 self.target = 1;
             }
-            Max => {
-                self.target = self.duty_cycles.len() - 1;
+            A::Max => {
+                if !self.available_actions.contains(&action) {
+                    panic!("Action not available fror device");
+                }
+                self.target = self.get_max_usable_duty_cycle_index();
             }
-            Reverse => {
+            A::Reverse => {
+                if !self.available_actions.contains(&action) {
+                    panic!("Action not available fror device");
+                }
                 self.reversed = !self.reversed;
             }
-            Set { .. } => {
-                let target = action.get_target().unwrap();
-                self.target = if target > self.duty_cycles.len() - 1 {
-                    self.duty_cycles.len() - 1
-                } else {
-                    target
-                };
+            A::Set(v) => {
+                if !self.available_actions.contains(&Action::Set(0)) {
+                    panic!("Action not available fror device");
+                }
+                self.target = v.min(self.get_max_usable_duty_cycle_index()); 
             }
         }
         self.action = action;
@@ -417,10 +425,33 @@ impl Device {
     }
 
     pub fn get_duty_cycle(&self) -> u32 {
-        self.duty_cycles[self.target]
+        self.duty_cycles[self.target].unwrap()
     }
 }
 
+pub struct Devices {
+    pub devices: Arc<Mutex<Vec<Device>>>,
+}
+
+impl Devices {
+    fn append(&mut self, other: &mut Self) {
+        let mut self_guard = self.devices.lock().unwrap();
+        let mut other_guard = other.devices.lock().unwrap();
+        self_guard.append(&mut other_guard);
+    }
+
+    fn new(devices: Arc<Mutex<Vec<Device>>>) -> Self {
+        Self { devices }
+    }
+
+    pub fn clone(&self) -> Self {
+        Self {
+            devices: Arc::clone(&self.devices)
+        }
+    }
+}
+
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -673,4 +704,4 @@ mod tests {
 //     Down,
 //     Min,
 //     Max,
-//     Set,
+//     Set,*/
